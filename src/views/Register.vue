a3594205ea41db972b96f76254f06caf5e585bf7
Vinícius <v.f.diehl@gmail.com>
2019-05-28 15:46:45 -0300

    Files

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -37,17 +37,15 @@
                     v-model="usuario.password_confirmation"
                     label="Password Confirmation"
                     type="password"
-                  ></v-text-field>                  
-                  <input
-                    prepend-icon="camera_enhance"
-                    type="file"
-                    @change="uploadImage"
-                  />
+                  ></v-text-field>
+                  <input type="file" name="imgprofile" @change="saveImageProfile"/>
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary"><router-link to="/login">Login</router-link></v-btn>
+                <v-btn color="primary">
+                  <router-link to="/login">Login</router-link>
+                </v-btn>
                 <v-btn color="success" @click="register()">Register</v-btn>
               </v-card-actions>
             </v-card>
@@ -67,40 +65,54 @@
   </v-app>
 </template>
 <script>
-import VueElementLoading from "vue-element-loading"
-import axios from 'axios'
+import VueElementLoading from "vue-element-loading";
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
+import axios from "axios";
 export default {
-    name: 'register',
-    components: { VueElementLoading },
-    data () {
-        return {
-            usuario: {
-                name: '',
-                email: '',
-                password: '',
-                password_confirmation: '',
-            },
-            isActive: false,
-        }
-    },
-    methods: {
-        uploadImage(e){
-            let file = e.target.files || e.dataTransfer.files
-            console.log(file)
-        },
-        register() {
-            const URL_API = "http://laravel-register-app.test/api/create";
-            axios.post('http://laravel-register-app.test/api/create', {
-                name: this.usuario.name,
-                email: this.usuario.email,
-                password: this.usuario.password,
-                password_confirmation: this.usuario.password_confirmation,
-                file: this.usuario.file
-            }).then(response => {
-                console.log(response.data)
-            })
-        }
+  name: "register",
+  components: { VueElementLoading, vueDropzone: vue2Dropzone },
+  data() {
+    return {
+      usuario: {
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+        imgprofile: ""
+      },
+      isActive: false,
     }
-}
+  },
+  methods: {
+    saveImageProfile(e) {
+      const file = e.target.files || e.dataTransfer.files
+      if (!files.length)
+        return;
+
+      this.createImage(files[0]);
+    },
+    createImage(file) {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        this.imgprofile = e.target.result
+      }
+      reader.readAsDataURL(file)
+    },
+    register() {
+      axios
+        .post("http://laravel-register-app.test/api/create", {
+          name: this.usuario.name,
+          email: this.usuario.email,
+          password: this.usuario.password,
+          password_confirmation: this.usuario.password_confirmation,
+          imgprofile: this.usuario.imgprofile
+        })
+        .then(response => {
+          console.log(response.data);
+        });
+    }
+  }
+};
 </script>
 
